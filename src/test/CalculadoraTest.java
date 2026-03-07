3edc38d6f8d6e2f0dad2c2b8b96640194e503b1d
JPedro-ESP <jpgonzalezhens@gmail.com>
2026-03-07 11:28:59 +0100

    primeros test

---
+++ src/test/CalculadoraTest.java
@@ -0,0 +1,75 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+import modelo.Calculadora;
+
+public class CalculadoraTest {
+
+    @Test
+    void testEstudioAssert1(){
+
+        // SI SE REALIZA ASI LOS TEST, UN ASSERT DEBAJO DE OTRO, SI FALLA UNO LOS SIGUIENTES NO SE EJECUTAN
+        System.out.println("COMIENZO TEST 1");
+        Calculadora calculadora = new Calculadora();
+
+        int suma = calculadora.suma(10, 15);
+        int esperado = 25;
+        assertEquals(esperado, suma, "Positivo y positivo");
+        System.out.println("SUMA POSITIVO Y POSITIVO FUNCIONA");
+
+        suma = calculadora.suma(10, -15);
+        esperado = -5;
+        assertEquals(esperado, suma, "Positivo y negativo");
+        System.out.println("SUMA POSITIVO Y NEGATIVO FUNCIONA");
+
+        suma = calculadora.suma(-10, -15);
+        esperado = -25;
+        assertEquals(esperado, suma, "Negativo y negativo");
+        System.out.println("SUMA NEGATIVO Y NEGATIVO FUNCIONA");
+
+    }
+
+    @Test
+    void testEstudioAssert2(){
+
+        System.out.println("COMIENZO TEST 2");
+        Calculadora calculadora = new Calculadora();
+
+        int suma = calculadora.suma(10, 5);
+        assertTrue(suma > 10);
+        System.out.println("CONDICION TRUE FUNCIONA");
+
+        assertFalse(suma < 10);
+        System.out.println("CONDICION FALSE FUNCIONA");
+
+        assertNotNull(calculadora, "La calculadora esta instanciada");
+        System.out.println("CONDICION NO SER NULL FUNCIONA");
+
+        calculadora = null;
+        assertNull(calculadora, "La calculadora no esta instanciada");
+        System.out.println("CONDICION DE SER NULL FUNCIONA");
+
+    }
+
+    @Test
+    void testEstudioAssert3(){
+
+        System.out.println("COMIENZO TEST 3");
+        Calculadora calculadora = new Calculadora();
+
+        double suma = calculadora.suma(3.51, 1);
+        double esperado = 4.5;
+
+        assertEquals(esperado, suma, 0.01, "SUMA DECIMALES");
+        System.out.println("SUMA DECIMALES FUNCIONA");
+
+    }
+
+}
